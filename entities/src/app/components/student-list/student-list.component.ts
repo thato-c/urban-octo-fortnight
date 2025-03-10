c09e9732155e73e3876af4cedcc62fde71c0b2f6
Thato <t.chitja369@gmail.com>
2025-03-10 10:38:03 +0200

    Add Hover effect to StudentListComponent

--- entities/src/app/components/student-list/student-list.component.ts
+++ entities/src/app/components/student-list/student-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Student } from '../../student.model'
 import { StudentFormComponent } from "../student-form/student-form.component";
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-list',
@@ -15,6 +16,11 @@ export class StudentListComponent {
     {id: 2, firstName: 'Isaac', lastName: 'Newton'},
     {id: 3, firstName: 'Erwin', lastName: 'Schrodinger'},
   ]
+  selectedStudent: any;
+  selectedStudentForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
   addStudent(newStudent: Student){
     this.students.push(newStudent);
@@ -22,5 +28,28 @@ export class StudentListComponent {
 
   deleteStudent(id: number): void{
     this.students = this.students.filter(student => student.id !== id);
+  }
+
+  editStudent(student: any): void{
+    this.selectedStudent = student;
+    this.selectedStudentForm = new FormGroup({
+      firstName: new FormControl(student.firstName),
+      lastName: new FormControl(student.lastName),
+    });
+  }
+
+  updateStudent(){
+    if (this.selectedStudent){
+      this.selectedStudent.firstName = this.selectedStudentForm.value.firstName;
+      this.selectedStudent.lastName = this.selectedStudentForm.value.lastName;
+
+      this.selectedStudentForm.reset();
+      this.selectedStudent = null;
+    }
+  }
+
+  cancelEdit(): void{
+    this.selectedStudentForm.reset();
+    this.selectedStudent = null;
   }
 }
